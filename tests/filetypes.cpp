282a4b3b06ced751ebaa6a541bb398d5cb8220e5
Goshenko <gbiwnl@gmail.com>
2026-04-05 22:31:34 -0400

    Add tests comments

---
+++ tests/filetypes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+// Use automation tests for various different file types:
+// Audio: .mp3
+// Video: .mp4
+// Pictures: .jpeg, .png
+// Documents: .pdf
+// Text-Based Files: .txt, .py, .cpp, etc.
+using namespace std;
+
+int main () {
+
+	return 0;
+
+}
